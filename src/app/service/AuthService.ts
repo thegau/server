ebd97e98171345c4c14f9267b8571c05ff0cfcef
vupham <aflyduck@outlook.com>
2022-12-19 00:04:46 +0700

    inject dependencies

--- src/app/service/AuthService.ts
+++ src/app/service/AuthService.ts
@@ -1,10 +1,24 @@
+import {autoInjectable} from "tsyringe"
+import {User} from "../model"
+import {IUserRepository, UserRepository} from "../repository"
+import {AuthToken} from "../@type"
+import {IJwtHelper, JwtHelper} from "../helper"
+
 interface IAuthService
 {
-
+   signUp(user: User): Promise<AuthToken>
 }
 
+@autoInjectable()
 class AuthService implements IAuthService
 {
+   private readonly userRepo: IUserRepository
+   private readonly jwtHelper: IJwtHelper
+
+   constructor(userRepo: UserRepository, jwtHelper: JwtHelper) {
+      this.userRepo = userRepo
+      this.jwtHelper = jwtHelper
+   }
 
    private createAuthToken(user: User): AuthToken {
       return {
