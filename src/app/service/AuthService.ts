3070694956a0bff7c20bf1db6f191d26c595eaca
vupham <aflyduck@outlook.com>
2022-12-19 00:04:26 +0700

    create createAuthToken function

--- src/app/service/AuthService.ts
+++ src/app/service/AuthService.ts
@@ -6,6 +6,17 @@ interface IAuthService
 class AuthService implements IAuthService
 {
 
+   private createAuthToken(user: User): AuthToken {
+      return {
+         accessToken: this.jwtHelper.encode(user),
+         refreshToken: this.jwtHelper.encode(user)
+      }
+   }
+
+   async signUp(newUser: User): Promise<AuthToken> {
+      newUser = await this.userRepo.createOne(newUser)
+      return this.createAuthToken(newUser)
+   }
 }
 
 export {AuthService, IAuthService}
