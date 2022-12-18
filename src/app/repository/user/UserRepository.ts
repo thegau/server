98dc5f739e3b89b96c25a3d707283e0b063193fe
vupham <aflyduck@outlook.com>
2022-12-18 21:54:33 +0700

    create createOne function

--- src/app/repository/user/UserRepository.ts
+++ src/app/repository/user/UserRepository.ts
@@ -1,14 +1,22 @@
 import {autoInjectable} from "tsyringe"
+import {AbstractRepository} from "../AbstractRepository"
+import {User} from "../../model"
 
 interface IUserRepository
 {
-
+   createOne(newUser: User): Promise<User>
 }
 
 @autoInjectable()
-class UserRepository implements IUserRepository
+class UserRepository extends AbstractRepository<User> implements IUserRepository
 {
+   constructor() {
+      super(User)
+   }
 
+   createOne(newUser: User): Promise<User> {
+      return this.self.save(newUser)
+   }
 }
 
 export {UserRepository, IUserRepository}
