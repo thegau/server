bdd39a62d2ef547a9726c7638eb39f380361e2ae
vupham <aflyduck@outlook.com>
2022-12-19 00:05:08 +0700

    create signUp function

--- src/app/controller/AuthController.ts
+++ src/app/controller/AuthController.ts
@@ -1,15 +1,34 @@
 import {AbstractController} from "./AbstractController"
-import {Router} from "express"
+import {Request, Response, Router} from "express"
+import {AuthService, IAuthService} from "../service"
+import {autoInjectable} from "tsyringe"
+import {AuthToken} from "../@type"
+import {User} from "../model"
 
+@autoInjectable()
 class AuthController extends AbstractController
 {
-   constructor() {
-      super();
+   private readonly authService: IAuthService
+
+   constructor(authService: AuthService) {
+      super()
+      this.authService = authService
    }
 
    routes = (): Router => {
+      this.router.post("/sign-up", this.signUp)
       return this.router
    }
+
+   signUp = async (req: Request, res: Response): Promise<Response<AuthToken>> => {
+      const newUser: User = new User()
+      newUser.username = req.body.username
+      newUser.email = req.body.email
+      newUser.password = req.body.password
+
+      return res.send(await this.authService.signUp(newUser))
+   }
+
 }
 
 export {AuthController}
