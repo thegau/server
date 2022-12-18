b3efb93f0b7a41ae232f0990479fd4d46373b024
vupham <aflyduck@outlook.com>
2022-12-18 22:51:32 +0700

    create IJwtHelper

--- src/app/helper/JwtHelper.ts
+++ src/app/helper/JwtHelper.ts
@@ -1,7 +1,15 @@
 import {autoInjectable} from "tsyringe"
+import {sign, SignOptions} from "jsonwebtoken"
+import {JwtConfig} from "../../env/JwtConfig"
+
+interface IJwtHelper
+{
+   encode(payload: any, options?: SignOptions): any
+   decode(): any
+}
 
 @autoInjectable()
-class JwtHelper
+class JwtHelper implements IJwtHelper
 {
 
 }
