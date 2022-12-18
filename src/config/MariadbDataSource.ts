69eb9548b3b800f3d2f4b277a27c814e96a77fe7
vupham <aflyduck@outlook.com>
2022-12-18 23:38:01 +0700

    add some entities

--- src/config/MariadbDataSource.ts
+++ src/config/MariadbDataSource.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import {DatabaseConfig} from "../env"
+import {Profile, User} from "../app/model";
+
 
 const MariadbDataSource = new DataSource({
     ...DatabaseConfig,
     synchronize: true,
     logging: false,
-    entities: [],
+    entities: [User, Profile],
     migrations: [],
     subscribers: [],
 })
